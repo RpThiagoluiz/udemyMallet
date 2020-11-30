060e38a44d7865191f903c824b107aa58cb65bad
Thiago <rp.thiagoluiz@gmail.com>
2020-11-30 15:45:03 -0300

    upGrade Grphics

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -10,7 +10,16 @@ import {
 } from 'recharts'
 
 
-import { Container, ChartContainer } from './styles'
+import formatCurrency from '../../utils/formatCurrency';
+
+import { 
+   Container, 
+   ChartContainer,
+   Header,
+   LegendContainer,
+   Legend
+
+} from './styles'
 
 interface IHistoryBoxProps {
    data: {
@@ -30,7 +39,27 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
    lineColorAmountOutPut
 }) => (
       <Container>
-         <h2>Historico de saldo</h2>
+         <Header>
+            <h2>Historico de saldo</h2>
+            <LegendContainer>
+              <Legend color={lineColorAmountEntry}>
+                  <div></div>
+                  <span>Entradas</span>
+               </Legend>
+
+              <Legend color={lineColorAmountOutPut}>
+                  <div></div>
+                  <span>Saidas</span>
+               </Legend>
+
+            </LegendContainer>
+               
+            
+            
+               
+
+
+         </Header>
 
 
          <ChartContainer>
@@ -38,7 +67,7 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
                <LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 20 }}>
                   <CartesianGrid strokeDasharray="3 3" stroke="#cecece" />
                   <XAxis dataKey="month" stroke="#cecece" />
-                  <Tooltip />
+                  <Tooltip formatter={(value) => formatCurrency(Number(value))}/>
                   <Line type="monotone" dataKey="amountEntry" name="Entradas" stroke={lineColorAmountEntry} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
                   <Line type="monotone" dataKey="amountOutPut" name="Saidas" stroke={lineColorAmountOutPut} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
 
--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface ILegendProps {
+   color:string
+}
+
 export const Container = styled.div`
    width: 100%;
    
@@ -14,15 +18,55 @@ export const Container = styled.div`
 
    border-radius: 7px;
 
-   > h2 {
-      margin-bottom: 20px;
-      padding-left: 16px;
-
-   }
- 
 `
 
 export const ChartContainer = styled.div`
    flex:1;
    height: 260px;
+`
+export const Header = styled.header`
+   width: 100%;
+   display:flex;
+   justify-content: space-between;
+
+
+   > h2 {
+      margin-bottom: 20px;
+      padding-left: 16px;
+
+   }
+`
+
+
+export const LegendContainer = styled.ul`
+   list-style:none;
+   display:flex;
+
+
+`
+
+export const Legend = styled.li<ILegendProps>`
+   display: flex;
+   align-items: center;
+
+   margin-bottom: 7px;
+   margin-left: 7px;
+   padding-right:17px;
+
+    > div {
+       background-color: ${props=> props.color};
+
+       width: 40px;
+       height: 40px;
+       border-radius: 5px;
+
+       font-size: 14px;
+       line-height:40px;
+       text-align:center;
+    }
+
+    > span {
+       margin-left: 5px;
+    }
+
 `
