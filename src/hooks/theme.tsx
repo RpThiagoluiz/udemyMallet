18c3f22b6aa6f15dc7dadc956c1a3e04e1e60c4f
Thiago <rp.thiagoluiz@gmail.com>
2020-12-09 15:02:45 -0300

    ToggleTheme Work - Local

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -27,13 +27,23 @@ interface ITheme {
 
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext)
 const ThemeProvider: React.FC = ({children}) => {
-    const [theme,setTheme] = useState<ITheme>(dark)
+    const [theme,setTheme] = useState<ITheme>(() => {
+        const themeSaved = localStorage.getItem('@minha-carteira:theme')
+
+        if(themeSaved) {
+            return JSON.parse(themeSaved)
+        } else {
+            return dark
+        }
+    })
 
     const toggleTheme = () => {
         if(theme.title === 'dark') {
             setTheme(light)
+            localStorage.setItem('@minha-carteira:theme',JSON.stringify(light)) //@pra guardar o theme, s[o pra ficar bunitim e nao ser sobreescrita]
         } else {
             setTheme(dark)
+            localStorage.setItem('@minha-carteira:theme',JSON.stringify(dark))
         }
     }
 
